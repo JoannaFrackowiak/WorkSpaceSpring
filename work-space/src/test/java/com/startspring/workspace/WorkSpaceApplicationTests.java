cecaea8edc54b09a11886c54c9af6f354af008b0
Joanna Frąckowiak <joanna.maria.frackowiak@gmail.com>
2020-12-02 19:08:13 +0100

    First Commit

---
+++ work-space/src/test/java/com/startspring/workspace/WorkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startspring.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
